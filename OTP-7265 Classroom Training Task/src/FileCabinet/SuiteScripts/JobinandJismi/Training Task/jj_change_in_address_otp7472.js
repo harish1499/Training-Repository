3f8f8108d237095c0cd75273d43aab458ad47066
harish1499 <harish.kanna@jobinandjismi.com>
2024-07-05 15:23:55 +0530

    OTP-7472 Created

---
+++ OTP-7265 Classroom Training Task/src/FileCabinet/SuiteScripts/JobinandJismi/Training Task/jj_change_in_address_otp7472.js
@@ -0,0 +1,89 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+/************************************************************************************************************************************
+ *********************
+ *SuiteScript Training
+ *
+ * OTP:7472 : Identify Change in Address
+ * 
+ * 
+ ************************************************************************************************************************************
+ **************
+ *
+ * Author : Jobin and Jismi IT Services
+ * 
+ * Date Created : 05-July-2024
+ * 
+ * Description : This script describe that if there is any change happens in existing address or new address is added in the customer
+ *               record it automates to check the custom checkbox value as true.
+ * 
+ * REVISION HISTORY
+ * 
+ * @version 1.0 OTP-7472 : 05-July-2024 Created the initial built by JJ0333
+ * 
+ * 
+ **************************************************************************************************************************************
+ *************/
+
+define(['N/record'],
+/**
+ * @param{record} record
+ */
+function(record) {
+
+    function fieldChanged(scriptContext) {
+       
+        let currentRec = scriptContext.currentRecord;
+        let fieldId = scriptContext.fieldId;
+        let sublistId = scriptContext.sublistId;
+
+        //If changes happens in existing address or new address added the custom checkbox is checked.
+        if(sublistId === 'addressbook' && fieldId === 'addressbookaddress_text'){
+            let customCheckBox = currentRec.setValue({
+                fieldId: 'custentity_jj_address_changed',
+                value: true
+            });
+            console.log("Line address is changed, Checkbox checked");
+        }else{
+            let customCheckBox = currentRec.setValue({
+                fieldId: 'custentity_jj_address_changed',
+                value: false
+            });
+        }
+    }
+    
+    return {
+        fieldChanged: fieldChanged
+    };
+    
+});
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ScheduledScript
+ */
+
+define(['N/search'], function(search) {
+
+    function deleteSavedSearch() {
+        var savedSearchId = 'customsearch_my_saved_search'; // Replace with your saved search ID
+        
+        try {
+            // Delete the saved search
+            search.delete({
+                id: 89
+            });
+
+            log.audit('Success', 'Saved search deleted successfully');
+        } catch (e) {
+            log.error('Error', 'Error deleting saved search: ' + e.message);
+        }
+    }
+
+    return {
+        execute: deleteSavedSearch
+    };
+
+});
